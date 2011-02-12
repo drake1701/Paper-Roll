ce53ba38333e689b9195835412143d4bfa441cc0
Dennis <dennis@drogers.net>
2011-02-12 01:21:45 +0000

    Added comments to bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,12 +1,15 @@
 <?php
+/*
+ *  @author  Dennis Rogers
+ */
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 	protected function _initDoctype()
 	{
 		$this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+        	$view = $this->getResource('view');
+        	$view->doctype('XHTML1_STRICT');
 	}
 }
 
